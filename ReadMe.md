178b2687f7d9e350ad5027a7c8b2084822527ec6
郭俊 <guojun@yunlizhihui.com>
2023-07-17 16:32:28 +0800

    first commit from local

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+动物庄园
+1. 可以建多个module
+2. 两级pom.xml
+3. 每个module独立引jar包，不能互相干扰，便于以后新增module
---
+++ nacos-spring-boot-config-example/src/main/java/com/animal/farm/nacos/SpringbootSampleApplication.java
@@ -0,0 +1,16 @@
+package com.animal.farm.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guojun
+ * @date 2023/7/17 16:21
+ */
+@SpringBootApplication
+public class SpringbootSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSampleApplication.class, args);
+  }
+}
