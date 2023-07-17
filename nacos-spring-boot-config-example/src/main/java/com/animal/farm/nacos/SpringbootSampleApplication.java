59bd0d7c06e1d9e66868a9e832694e48bae26d91
郭俊 <guojun@yunlizhihui.com>
2023-07-17 19:37:27 +0800

    nacos自动配置

--- nacos-spring-boot-config-example/src/main/java/com/animal/farm/nacos/SpringbootSampleApplication.java
+++ nacos-spring-boot-config-example/src/main/java/com/animal/farm/nacos/SpringbootSampleApplication.java
@@ -3,11 +3,14 @@ package com.animal.farm.nacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+
 /**
  * @author guojun
  * @date 2023/7/17 16:21
  */
 @SpringBootApplication
+@NacosPropertySource(dataId = "example")
 public class SpringbootSampleApplication {
 
   public static void main(String[] args) {
